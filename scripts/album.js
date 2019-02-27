1ecc494cc2213a45460fe67e7fb2b17fe63ecf62
andres alcocer <andresalcocer7@yahoo.com>
2019-02-27 13:05:24 -0500

    displayed only the last album

--- scripts/album.js
+++ scripts/album.js
@@ -1,33 +1,41 @@
 var createSongRow = function (songNumber, songName, songLength) {
   var template =
-     '<tr class="album-view-song-item">'
-   + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-   + '  <td class="song-item-title">' + songName + '</td>'
-   + '  <td class="song-item-duration">' + songLength + '</td>'
-   + '</tr>'
-   ;
+    '<tr class="album-view-song-item">'
+    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+    + '  <td class="song-item-title">' + songName + '</td>'
+    + '  <td class="song-item-duration">' + songLength + '</td>'
+    + '</tr>'
+    ;
 
-   var $row = $(template);
+  var $row = $(template);
 
-   return $row;
+  return $row;
 };
 
-var setCurrentAlbum = function(album) {
+var setCurrentAlbum = function (album) {
   var $albumTitle = $('.album-view-title');
   var $albumArtist = $('.album-view-artist');
   var $albumReleaseInfo = $('.album-view-release-info');
   var $albumImage = $('.album-cover-art');
   var $albumSongList = $('.album-view-song-list');
 
-  $albumTitle.text(album.title);
-  $albumArtist.text(album.artist);
-  $albumReleaseInfo.text(album.year + ' ' + album.label);
-  $albumImage.attr('src', album.albumArtUrl);
 
   $albumSongList.empty();
 
-  for (var i = 0; i < album.songs.length; i++) {
-    var $songRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-    $albumSongList.append($songRow);
-  }
+  album.forEach(function (album, idx) {
+    $albumTitle.text(album.title);
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + ' ' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
+
+
+    console.log(album);
+    album.songs.forEach(function (song) {
+      var $songRow = createSongRow(idx + 1, song.title, song.duration);
+      $albumSongList.append($songRow);
+    })
+
+  })
 };
+
+setCurrentAlbum(albums);
